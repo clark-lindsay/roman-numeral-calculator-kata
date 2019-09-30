90fef98f033610fdbf42ab0ea18abccec98fc96e
clark-lindsay <clarklindsay96@gmail.com>
2019-09-30 16:53:28 -0400

    all of the work done while I was on site at pillar

--- romanNumeralToInt.js
+++ romanNumeralToInt.js
@@ -1,5 +1,36 @@
 function romanNumeralToInt(romanNumeral) {
-    return 1;
+    const upperCaseRomanNumeral = romanNumeral.toUpperCase();
+    const romanNumeralToIntLookupTable = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    const result = romanNumeralToIntLookupTable[upperCaseRomanNumeral];
+
+    if (upperCaseRomanNumeral.length > 1) {
+        const [char1Value, char2Value] = [romanNumeralToIntLookupTable[upperCaseRomanNumeral[0]], romanNumeralToIntLookupTable[upperCaseRomanNumeral[1]]];
+        if (char2Value > char1Value) {
+            return char2Value - char1Value;
+        }
+    }
+
+    if (!result) {
+        throw new InvalidRomanNumeralError('Valid roman numerals are IVX...');
+    }
+
+    return result;
 }
 
-module.exports = romanNumeralToInt;
+class InvalidRomanNumeralError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "InvalidRomanNumeralError";
+    }
+}
+
+module.exports = { romanNumeralToInt, InvalidRomanNumeralError };
--- romanNumeralToInt.test.js
+++ romanNumeralToInt.test.js
@@ -1,15 +1,36 @@
-const romanNumeralToInt = require('./romanNumeralToInt');
+const { romanNumeralToInt, InvalidRomanNumeralError } = require('./romanNumeralToInt');
 
 describe('Roman numeral to integer translator', () => {
-    it('can convert an "I" into a 1', () => {
-        expect(romanNumeralToInt('I')).toEqual(1);
-    });
-
     it('is case insensitive for handling conversions', () => {
         expect(romanNumeralToInt('i')).toEqual(1);
+        expect(romanNumeralToInt('l')).toEqual(50);
+        expect(romanNumeralToInt('m')).toEqual(1000);
     });
 
-    /*it('it can convert a "V" into a 5', () => {
-        expect(romanNumeralToInt('V'))
-    });*/
+    it('can convert a single valid roman numeral into its equivalent number', () => {
+        expect(romanNumeralToInt('I')).toEqual(1);
+        expect(romanNumeralToInt('L')).toEqual(50);
+        expect(romanNumeralToInt('M')).toEqual(1000);
+    });
+
+    it('should throw an error if given an invalid roman numeral', () => {
+        expect(() => romanNumeralToInt('P')).toThrow(InvalidRomanNumeralError);
+    });
+
+    it('returns 4 when given IV', () => {
+        expect(romanNumeralToInt('IV')).toEqual(4);
+    });
+
+    it('returns 9 when given IX', () => {
+        expect(romanNumeralToInt('IX')).toEqual(9);
+    });
+
+    it('returns 49 when given IL', () => {
+        expect(romanNumeralToInt('IL')).toEqual(49);
+    });
+
+    it('can convert two character roman numeral strings into integers', () => {
+        expect(romanNumeralToInt('IV')).toEqual(4);
+        expect(romanNumeralToInt('XL')).toEqual(40);
+    });
 });
