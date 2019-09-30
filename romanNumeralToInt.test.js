7a30bd208991940ac953f3bee008d21343705f9d
clark-lindsay <clarklindsay96@gmail.com>
2019-09-30 14:42:57 -0400

    is case insensitive with respect to 1

--- romanNumeralToInt.test.js
+++ romanNumeralToInt.test.js
@@ -4,4 +4,12 @@ describe('Roman numeral to integer translator', () => {
     it('can convert an "I" into a 1', () => {
         expect(romanNumeralToInt('I')).toEqual(1);
     });
+
+    it('is case insensitive for handling conversions', () => {
+        expect(romanNumeralToInt('i')).toEqual(1);
+    });
+
+    /*it('it can convert a "V" into a 5', () => {
+        expect(romanNumeralToInt('V'))
+    });*/
 });
