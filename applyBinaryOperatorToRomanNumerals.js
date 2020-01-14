785999858c96a8396a02aa9a2976f21f6134a373
clark-lindsay <clarklindsay96@gmail.com>
2020-01-14 13:46:53 -0500

    can now apply subtraction to two roman numerals

--- applyBinaryOperatorToRomanNumerals.js
+++ applyBinaryOperatorToRomanNumerals.js
@@ -1,5 +1,6 @@
 import { romanNumeralToInt } from './romanNumeralToInt';
 
 export function applyBinaryOperatorToRomanNumerals(firstOperand, operator, secondOperand) {
+    if (operator === '-') return romanNumeralToInt(firstOperand) - romanNumeralToInt(secondOperand);
     return romanNumeralToInt(firstOperand) + romanNumeralToInt(secondOperand);
 }
--- applyBinaryOperatorToRomanNumerals.test.js
+++ applyBinaryOperatorToRomanNumerals.test.js
@@ -6,4 +6,10 @@ describe('applying binary operators to roman numerals', () => {
         expect(applyBinaryOperatorToRomanNumerals('X', '+', 'VI')).toEqual(16);
         expect(applyBinaryOperatorToRomanNumerals('MDL', '+', 'VIII')).toEqual(1558);
     });
+
+    it('can subtract one roman numeral from another', () => {
+        expect(applyBinaryOperatorToRomanNumerals('VII', '-', 'IV')).toEqual(3);
+        expect(applyBinaryOperatorToRomanNumerals('XLV', '-', 'VII')).toEqual(38);
+        expect(applyBinaryOperatorToRomanNumerals('MC', '-', 'D')).toEqual(600);
+    });
 });
